398109645258c6ab104334b6161216bffc03bb87
unknown <shvydchenkoandriy@gmail.com>
2023-01-09 14:45:03 +0200

    added button Add and Delete with functions

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,46 +1,56 @@
-import Head from "next/head";
-import Layout, { siteTitle } from "../components/layout";
-import utilStyles from "../styles/utils.module.css";
-import { getSortedPostsData } from "../lib/posts";
-import Link from "next/link";
-import Date from "../components/date";
+import { useState } from "react";
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
+export default function ToDo() {
+  // const startState = {
+  //   items: [
+  //     { title: "1", completed: false },
+  //     { title: "2", completed: false },
+  //     { title: "3", completed: false },
+  //   ],
+  // };
+  const [state, setState] = useState({ items: [], filter: "all" });
+
+  const handlerAddButt = () => {
+    let inputTxt = document.getElementById("textInp").value;
+    const newItem = {
+      title: inputTxt,
+      completed: false,
+      id: Math.ceil(Math.random() * 1000),
+    };
+    newItem.title !== ""
+      ? setState({ items: state.items.concat(newItem), filter: "all" })
+      : null;
+    inputTxt = document.getElementById("textInp").value = "";
   };
-}
 
-export default function Home({ allPostsData }) {
+  const handlerDeleteButt = (id) => {
+    const itemIdx = state.items.findIndex((item) => item.id === id);
+    state.items.splice(itemIdx, 1);
+    setState({ items: state.items });
+  };
+
+  const list = state.items.map((item) => {
+    return (
+      <div key={item.id}>
+        <input
+          type="checkbox"
+          // checked={item.completed === true ? "checked" : null}
+        ></input>
+        <p>{item.title}</p>
+        <button onClick={() => handlerDeleteButt(item.id)}>Delete</button>
+      </div>
+    );
+  });
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
+    <div>
+      <input typeof="text" id="textInp"></input>
+      <button onClick={() => handlerAddButt()}>Add</button>
+      <ul>{list}</ul>
+      <div>{state.items.length}</div>
+      <button>All</button>
+      <button>Completed</button>
+      <button>In progress</button>
+      <button>Clear</button>
+    </div>
   );
 }
